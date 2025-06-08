3e842d35c6b59de336f1b3263b51f40a52148c6a
siddhantantil39 <siddhantantil39@gmail.com>
2025-06-08 19:43:55 +0530

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,16 @@
+import { Providers } from "./provider/Providers";
+import { Analytics } from "@vercel/analytics/next"
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>
+          {children}
+          <Analytics/>
+        </Providers>
+      </body>
     </html>
   );
 }
