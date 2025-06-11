6abe6ef3b82472bb1b55a0db2eab62b89bb7641f
siddhantantil39 <siddhantantil39@gmail.com>
2025-06-12 01:38:56 +0530

    changed font of work

--- src/app/(pages)/work/page.tsx
+++ src/app/(pages)/work/page.tsx
@@ -3,16 +3,16 @@ import WorkData from "@/app/data/work/WorkData";
 export default function Work() {
     return (
         <div className="mx-auto py-8 items-left ">
-        <div className="mx-auto  ">
+        <div className="mx-auto ">
         <h1 className="text-4xl font-bold text-white mb-8">Work History</h1>
           <div className="">
             {Object.entries(WorkData).map((section, index) => (
               <div key={index} className="">
-                <h2 className="text-xl text-white font-semibold mb-4">{section[0]}</h2>
-                <div className="space-y-4">
+                <h2 className="text-xl text-white font-semibold mb-1">{section[0]}</h2>
+                <div className="py-1">
                     {Object.entries(section[1]).map((subSection, index) => (
-                        <div key={index} className="p-6">
-                            <h2 className="text-xl text-white font-semibold mb-4">{subSection[0]}</h2>
+                        <div key={index} className="p-2">
+                            <h2 className="text-xl text-white font-semibold mb-1">{subSection[0]}</h2>
                             <p className="text-gray-300 leading-relaxed">{subSection[1]}</p>
                         </div>
                     ))}
