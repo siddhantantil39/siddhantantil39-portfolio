f5374d288c1d50feb74d0b4da362eee9fb4d0469
Siddhant Antil <54893999+siddhantantil39@users.noreply.github.com>
2025-06-13 05:19:49 +0530

    Update EducationData.ts

--- src/app/data/education/EducationData.ts
+++ src/app/data/education/EducationData.ts
@@ -1,5 +1,5 @@
 const EducationData = {
-    description: "I graduated from Siddaganga Institute of Technolgy in 2022 as a major in Computer Science. I also learnt a lot of programing fundamentals from leetcode where you can find me at ",
+    description: "I graduated from Siddaganga Institute of Technolgy in 2022 in Computer Science. I also try to learn programing fundamentals from leetcode where you can find me at ",
     leetcodeLink: {
         text: "leetcode.com/u/siddhantantil39",
         href: "https://leetcode.com/u/siddhantantil39/"
